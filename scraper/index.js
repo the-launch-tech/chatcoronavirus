09ee80b97cb1ead156363c65e9c152eb7ad341aa
The Launch <daniel@thelaunch.tech>
2020-03-19 15:56:26 -0400

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -7,37 +7,18 @@ const axios = require('axios')
 
 const app = express()
 
-app.listen(5001, function() {
-  puppeteer.launch().then(browser => {
-    browser.newPage().then(page => {
-      page.goto('https://banned.video/').then(() => {
-        page
-          .evaluate(() => {
-            let els = document.querySelectorAll('.rc')
-            console.log('els', els)
-            return Array.from(els).map(el => {
-              const url = el.querySelector('.r a').href
-              const title = el.querySelector('.r h3').innerHTML
-              return { url, title }
-            })
-          })
-          .then(results => {
-            console.log('results', results)
-            browser.close()
-            console.log('browser.close()', browser.close())
-            results.map(result => {
-              console.log('result', result)
-              axios
-                .post(`${process.env.API_POST_RESOURCE}google`, result, {
-                  headers: {
-                    'x-scraper-token': process.env.SCRAPER_TOKEN,
-                  },
-                })
-                .then(console.log)
-                .catch(console.error)
-            })
-          })
-      })
-    })
+app.listen(5001, async function() {
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  await page.goto('https://www.brainyquote.com/topics/motivational-quotes')
+
+  await page.waitForSelector('.b-qt')
+
+  const titles = await page.$$eval('.b-qt', elements => {
+    return elements.map(item => item.textContent)
   })
+
+  console.log(titles)
+
+  await browser.close()
 })
