bc1b50e367ed3e91f7305e1d9388acaec5fd2ad5
The Launch <daniel@thelaunch.tech>
2020-03-19 15:59:08 -0400

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -8,17 +8,43 @@ const axios = require('axios')
 const app = express()
 
 app.listen(5001, async function() {
-  const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
-  await page.goto('https://www.brainyquote.com/topics/motivational-quotes')
+  console.log('app.listen 5001', new Date())
 
-  await page.waitForSelector('.b-qt')
-
-  const titles = await page.$$eval('.b-qt', elements => {
-    return elements.map(item => item.textContent)
-  })
-
-  console.log(titles)
-
-  await browser.close()
+  // cron.schedule('30 20 */1 * * *', async function() {
+  try {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+    console.log('browser', browser)
+    const page = await browser.newPage()
+    console.log('page', page)
+    await page.goto(
+      'https://www.google.com/search?hl=en&as_q=coronavirus+covid-19+updates&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr=&as_qdr=d&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights='
+    )
+    const results = await page.evaluate(() => {
+      let els = document.querySelectorAll('.rc')
+      console.log('els', els)
+      return Array.from(els).map(el => {
+        const url = el.querySelector('.r a').href
+        const title = el.querySelector('.r h3').innerHTML
+        return { url, title }
+      })
+    })
+    console.log('results', results)
+    browser.close()
+    console.log('cosed')
+    results.map(result => {
+      console.log('results', results)
+      axios
+        .post(`${process.env.API_POST_RESOURCE}google`, result, {
+          headers: {
+            'x-scraper-token': process.env.SCRAPER_TOKEN,
+          },
+        })
+        .then(console.log)
+        .catch(console.error)
+    })
+    console.log('mapped')
+  } catch (e) {
+    console.error(e)
+  }
+  // })
 })
