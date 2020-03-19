2c87de8edaa60f62923dbd4a0a3dc9d94a50aac0
The Launch <daniel@thelaunch.tech>
2020-03-19 12:25:45 -0400

    testing

--- app/Services/ImageUploader.php
+++ app/Services/ImageUploader.php
@@ -62,7 +62,7 @@ class ImageUploader {
   }
 
   public function resize() : self {
-    Debugbar::info('resize', $this->fullThumbnailPath);
+    Debugbar::info('resize', $this->fullThumbnailPath, Image::make($this->file));
     $imager = Image::make($this->file);
     Debugbar::info('$imager::make', $imager);
     $imager->resize($this->width, $this->height, function ($constraints) {
