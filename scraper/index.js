a4bc74d3ec09edd9173529a24ea62fb011ccd51e
The Launch <daniel@thelaunch.tech>
2020-03-19 15:45:27 -0400

    testing scraper

--- scraper/index.js
+++ scraper/index.js
@@ -7,37 +7,37 @@ const axios = require('axios')
 
 const app = express()
 
-app.listen(5001, function() {
+app.listen(5001, async function() {
   console.log('app.listen 5001', new Date())
 
-  cron.schedule('30 20 */1 * * *', async function() {
-    try {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.goto(
-        'https://www.google.com/search?hl=en&as_q=coronavirus+covid-19+updates&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr=&as_qdr=d&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights='
-      )
-      const results = await page.evaluate(() => {
-        let els = document.querySelectorAll('.rc')
-        return Array.from(els).map(el => {
-          const url = el.querySelector('.r a').href
-          const title = el.querySelector('.r h3').innerHTML
-          return { url, title }
-        })
-      })
-      browser.close()
-      results.map(result => {
-        axios
-          .post(`${process.env.API_POST_RESOURCE}google`, result, {
-            headers: {
-              'x-scraper-token': process.env.SCRAPER_TOKEN,
-            },
-          })
-          .then(console.log)
-          .catch(console.error)
-      })
-    } catch (e) {
-      console.error(e)
-    }
+  // cron.schedule('30 20 */1 * * *', async function() {
+  //   try {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(
+    'https://www.google.com/search?hl=en&as_q=coronavirus+covid-19+updates&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr=&as_qdr=d&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights='
+  )
+  const results = await page.evaluate(() => {
+    let els = document.querySelectorAll('.rc')
+    return Array.from(els).map(el => {
+      const url = el.querySelector('.r a').href
+      const title = el.querySelector('.r h3').innerHTML
+      return { url, title }
+    })
   })
+  browser.close()
+  results.map(result => {
+    axios
+      .post(`${process.env.API_POST_RESOURCE}google`, result, {
+        headers: {
+          'x-scraper-token': process.env.SCRAPER_TOKEN,
+        },
+      })
+      .then(console.log)
+      .catch(console.error)
+  })
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // })
 })
