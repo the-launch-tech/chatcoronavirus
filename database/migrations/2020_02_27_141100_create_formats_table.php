444943fe31d00330a688c668a0ef687d04d0a992
The Launch <daniel@thelaunch.tech>
2020-03-25 14:44:01 -0400

    reset database migrations

--- database/migrations/2020_02_27_141100_create_formats_table.php
+++ database/migrations/2020_02_27_141100_create_formats_table.php
@@ -22,21 +22,6 @@ class CreateFormatsTable extends Migration {
   }
 
   public function down() {
-    Schema::dropIfExists('malpractices');
-    Schema::dropIfExists('pins');
-    Schema::dropIfExists('subscriptions');
-    Schema::dropIfExists('aux');
-    Schema::dropIfExists('cures');
-    Schema::dropIfExists('realm_post');
-    Schema::dropIfExists('topic_post');
-    Schema::dropIfExists('post_user');
-    Schema::dropIfExists('comments');
-    Schema::dropIfExists('realms');
-    Schema::dropIfExists('topics');
-    Schema::dropIfExists('posts');
     Schema::dropIfExists('formats');
-    Schema::dropIfExists('cache');
-    Schema::dropIfExists('password_resets');
-    Schema::dropIfExists('users');
   }
 }
